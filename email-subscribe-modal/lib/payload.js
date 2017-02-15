1fa670648527d041e077dbf59ae264f0ee26d2ef
Samer Masterson <samer@samertm.com>
2017-02-14 20:58:52 -0800

    update again

--- email-subscribe-modal/lib/payload.js
+++ email-subscribe-modal/lib/payload.js
@@ -6,7 +6,7 @@ export var payload = `
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span id="close-button" aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title" id="myModalLabel">Sign up to learn about our next open rescue!</h4>
+  <h4 class="modal-title" id="myModalLabel">Sign up to learn about our<br>next open rescue!</h4>
           </div>
           <div class="modal-body">
             <div class="form-wrapper">

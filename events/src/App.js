4208664dc72653fe7d8a6feb34f51c18db60be52
Samer Masterson <samer@samertm.com>
2018-02-14 20:58:54 -0800

    Add "find your chapter" link, add analytics

--- events/src/App.js
+++ events/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { getEvents, logHit }  from './utils/api'
+import React from 'react';
+import { getEvents, logHit }  from './utils/api';
+import { EventLink } from './utils/EventLink';
 import './thirdparty/bootstrap/css/bootstrap.min.css';
 import EventList from './EventList';
 
@@ -29,10 +30,13 @@ class App extends React.Component {
     return (
       <div className="container">
         <h1 className="pageTitle">SF Bay Area Events</h1>
+        <p>
+          Not in the Bay Area? Find your chapter <EventLink analytics-type="find-chapter" href="https://www.directactioneverywhere.com/get-active-main#find-a-chapter">here</EventLink>.
+        </p>
         <EventList
           eventList={this.state.items}
           isLoading={this.state.isLoading} />
-        <div>Find all of our events <a href="https://www.facebook.com/pg/directactioneverywheresf/events/">here</a>.</div>
+        <div>Find all of our events <EventLink analytics-type="all-events" href="https://www.facebook.com/pg/directactioneverywheresf/events/">here</EventLink>.</div>
       </div>
 
     )
